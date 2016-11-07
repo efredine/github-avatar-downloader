5cd59dc9a1e8a19e8c1d4ad015d0f0a01686726d
Eric Fredine <eric.fredine@gmail.com>
2016-11-07 22:02:05 +0000

    Iterate over results and print avatar URL.

--- download_avatars.js
+++ download_avatars.js
@@ -6,7 +6,7 @@ const GITHUB_TOKEN = "63f30585505ab3b79fb77b07c7070483a0e9ab3e";
  * Retrieve a Github repo's contributors.
  * @param  {String} repoOwner name of the repositories owner
  * @param  {String} repoName name of the repository
- * @param  {Function} callback(result, error) is called on completion
+ * @param  {Function} callback(error, result) is called on completion where result is a JSON object.
  * @return {undefined}
  */
 function getRepoContributors(repoOwner, repoName, callback) {
@@ -20,29 +20,21 @@ function getRepoContributors(repoOwner, repoName, callback) {
     console.log("Error:", error);
     console.log('Response Status Code: ', response.statusCode);
     console.log('Response Status Message:', response.statusMessage);
-    console.log(JSON.parse(body));
+    if(!error && response.statusCode === 200) {
+      callback(error, JSON.parse(body));
+    } else {
+      callback(error, null);
+    }
   });
-    // .on('error', function (err) {
-    //   console.error(err);
-    //   throw err;
-    // })
-    // .on('response', function (response) {
-    //   console.log('Response Status Code: ', response.statusCode);
-    //   console.log('Response Status Message:', response.statusMessage);
-    //   if(response.statusCode === 200) {
-    //     console.log("Starting download...");
-    //   }
-    // });
-       // .on('data', function(chunk){
-       //  byteCount += chunk.length;
-       //  console.log(`${byteCount} of ${length}`);
-       // })
-       // .on('end', function(){
-       //  console.log('Download complete.');
-       // });
-       // .pipe(fs.createWriteStream('./future.jpg'));
-
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-getRepoContributors(process.argv[2], process.argv[3]);
+function testCallBack(error, result) {
+  console.log("error:", error);
+  if(!error && result) {
+    result.forEach(repoResult => {
+      console.log(repoResult["avatar_url"]);
+    });
+  }
+}
+getRepoContributors(process.argv[2], process.argv[3], testCallBack);
