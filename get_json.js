625df3b9c25ed6c07cb1fde67e4d96e01d2363ae
Eric Fredine <eric.fredine@gmail.com>
2016-11-08 02:04:39 +0000

    Changed logging of error handling for get_json.

--- get_json.js
+++ get_json.js
@@ -1,14 +1,13 @@
 const request = require('request');
 
 module.exports = function getJSON(options, callback) {
-  console.log(options.url);
   request.get(options, (error, response, body) => {
-    console.log("Error:", error);
-    console.log('Response Status Code: ', response.statusCode);
-    console.log('Response Status Message:', response.statusMessage);
     if(!error && response.statusCode === 200) {
       callback(error, JSON.parse(body));
     } else {
+      console.log(options.url);
+      console.log(response.statusCode, response.statusMessage);
+      console.log(body);
       callback(error, null);
     }
   });
