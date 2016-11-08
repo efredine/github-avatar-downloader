88e066ac1e86322dfcbfa27f4889c88a5e822392
Eric Fredine <eric.fredine@gmail.com>
2016-11-08 00:52:44 +0000

    github api module throws an error if not properly configured.

--- github_api.js
+++ github_api.js
@@ -1,13 +1,32 @@
 require('dotenv').config();
+const user = process.env.GITHUB_USER;
+const token = process.env.GITHUB_TOKEN;
 
-function getHost() {
-  return `https://${process.env.GITHUB_USER}:${process.env.GITHUB_TOKEN}@api.github.com`;
+if(!user || !token) {
+  throw new Error("You have to configure GITHUB_USER and GITHUB_TOKEN");
 }
 
+/**
+ * Returns the base URL for github with user and token configured.
+ * @return {String}
+ */
+function getHost() {
+  return `https://${user}:${token}@api.github.com`;
+}
+
+/**
+ * @param  {String} path to append.
+ * @return {Object} options object configured with user-agent and github URL.
+ */
 function getOptions(path) {
   return {url: getHost() + path, headers: {'User-Agent': 'github-avatar-downloader'}};
 }
 
+/**
+ * @param  {String} user id of repo owner
+ * @param  {String} name of the repo
+ * @return {String} path for contirbutors to the repo
+ */
 function getContributorsPath(repoOwner, repoName) {
   return `/repos/${repoOwner}/${repoName}/contributors`;
 }
